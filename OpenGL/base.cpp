55f577e7e76319c2f9333132fc6b92e87dfa27f9
PurpleSwords <38780499+PurpleSwords@users.noreply.github.com>
2020-06-13 01:16:34 +0800

    修改结构

--- OpenGL/base.cpp
+++ OpenGL/base.cpp
@@ -3,7 +3,6 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-//顶点着色器
 extern const char *vertexShaderSource = "#version 330 core\n"
 	"layout (location = 0) in vec3 aPos;\n"
 	"void main()\n"
@@ -11,7 +10,6 @@ extern const char *vertexShaderSource = "#version 330 core\n"
 	"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 	"}\0";
 
-//片段着色器
 extern const char *fragmentShaderSource = "#version 330 core\n"
 	"out vec4 FragColor;\n"
 	"void main()\n"
--- OpenGL/base.h
+++ OpenGL/base.h
@@ -10,6 +10,8 @@ extern const char *vertexShaderSource;
 //片段着色器
 extern const char *fragmentShaderSource;
 
+//修改窗体大小时的回调
 void framebuffer_size_callback(GLFWwindow* window, int width, int heigh);
 
+//处理按键输入
 void processInput(GLFWwindow* window);
