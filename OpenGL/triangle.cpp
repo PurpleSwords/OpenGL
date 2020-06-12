4a2e5382169ff957960379917e194fbcfb4b17d2
PurpleSwords <38780499+PurpleSwords@users.noreply.github.com>
2020-06-12 22:23:23 +0800

    初步绘制三角形

--- OpenGL/triangle.cpp
+++ OpenGL/triangle.cpp
@@ -13,7 +13,7 @@ const char *vertexShaderSource = "#version 330 core\n"
 
 //片段着色器
 const char *fragmentShaderSource = "#version 330 core\n"
-	"out vec4 FragColor\n"
+	"out vec4 FragColor;\n"
 	"void main()\n"
 	"{\n"
 	"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
@@ -32,6 +32,11 @@ void processInput(GLFWwindow* window)
 	}
 }
 
+/*
+	绘制多个物体：
+	1、生成/配置所有的VAO、VBO和属性指针，并储存
+	2、绘制时拿出相应的VAO并绑定，绘制结束后再解绑
+*/
 int main()
 {
 	glfwInit();
@@ -41,7 +46,7 @@ int main()
 	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
 	//创建窗口对象
-	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "创建三角形", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -60,16 +65,6 @@ int main()
 	glViewport(0, 0, 800, 600);
 	//每次窗口大小被调整时调用回调函数,对窗体进行调整
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-	float vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	0.5f, -0.5f, 0.0f,
-	0.0f, 0.5f, 0.0f
-	};
-	//顶点缓冲对象 Vertex Buffer Object,VBO
-	unsigned int VBO;
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
 	//创建顶点着色器
 	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -90,6 +85,13 @@ int main()
 	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
 	glCompileShader(fragmentShader);
+	//检查错误
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if(!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
 
 	//创建着色器程序
 	unsigned int shaderProgram = glCreateProgram();
@@ -98,9 +100,53 @@ int main()
 	glAttachShader(shaderProgram, fragmentShader);
 	glLinkProgram(shaderProgram);
 
+	//检查链接是否成功,注意第二个参数是链接
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINK_FAILED\n" << infoLog << std::endl;
+	}
+	//链接成功后不再需要着色器对象，可以删除
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 
+
+	float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	0.5f, -0.5f, 0.0f,
+	0.0f, 0.5f, 0.0f
+	};
+	/*	
+		顶点缓冲对象 Vertex Buffer Object,VBO
+		顶点数组对象 Vertex Array Object,VAO
+
+	*/
+	unsigned int VBO, VAO;
+	//glGenBuffers生成缓冲区对象名称，第一个参数表示生成个数
+	glGenBuffers(1, &VBO);
+	glGenVertexArrays(1, &VAO);
+	//1、绑定VAO
+	glBindVertexArray(VAO);
+	//2、复制顶点数组到缓冲中供OpenGL使用
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	//把用户定义的数据复制到当前绑定缓冲的函数
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//3、设置顶点属性指针
+	//如何解析顶点数据
+	/*
+		GLuint index, 指定要配置的顶点属性。之前在顶点着色器中使用了layout(location = 0)定义顶点属性的位置值，所以传0
+		GLint size, 顶点属性的大小，vec3由3个值组成
+		GLenum type, 指定数据的类型，vsc*都是由浮点数组成
+		GLboolean normalized, 数据标准化：True则将所有数据映射到-1到1，无符号数据映射到0到1
+		GLsizei stride, 步长设置，设置在连续的顶点属性组之间的间隔。顶点缓冲数据的存放形式类似于数组，连续存放
+		const void *pointer,表示位置数据在缓冲中起始位置的偏移量offset，在开头则为0.注意参数类型。
+	*/
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+
 	//glfwWindowShouldClose检查GLFW是否被要求退出
 	//渲染循环
 	while (!glfwWindowShouldClose(window))
@@ -112,6 +158,10 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		//4、渲染一个物体时要使用着色器程序：激活该对象，绘制物体
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);	//triangle,三角形
 
 		//交换颜色缓冲(双缓冲)
 		glfwSwapBuffers(window);
