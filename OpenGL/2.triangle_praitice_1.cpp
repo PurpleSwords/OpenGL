50edbb52fe860374c0b649dfba0158b09867f7dc
PurpleSwords <38780499+PurpleSwords@users.noreply.github.com>
2020-06-14 21:27:50 +0800

    三角形练习1

--- OpenGL/2.triangle_praitice_1.cpp
+++ OpenGL/2.triangle_praitice_1.cpp
@@ -6,7 +6,7 @@
 #include "base.h"
 
 
-int main()
+int main_2()
 {
 	//初始化
 	glfwInit();
@@ -33,5 +33,94 @@ int main()
 	glViewport(0, 0, 800, 600);
 	//每次窗口大小被调整时调用回调函数,对窗体进行调整
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	//顶点着色器
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR:SHADER:VERTEX:COMPLETE_FAILED\n" << infoLog << std::endl;
+	}
+	//片段着色器
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR:SHADER:FRAGMENT:COMPLETE_FAILED\n" << infoLog << std::endl;
+	}
+	//着色器程序
+	unsigned int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR:SHADER:PROGRAM:LINK_FAILED\n" << infoLog << std::endl;
+	}
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	float vertices[] = {
+		//第一个三角形
+		-0.9f, -0.5f, 0.0f,  //左下
+		 0.0f, -0.5f, 0.0f,  //右下
+		-0.45f, 0.5f, 0.0f,  //中上 
+		//第2个三角形
+		 0.0f, -0.5f, 0.0f,  //左下
+		 0.9f, -0.5f, 0.0f,  //右下
+		 0.45f, 0.5f, 0.0f   //左上 
+	};
+	//生成VAO、VBO
+	unsigned int VAO, VBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+	//线框模式，方便查看具体的生成
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	//渲染循环
+	while (!glfwWindowShouldClose(window))
+	{
+		processInput(window);
+
+		glClearColor(0.2f, 0.3f, 0.4f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//使用glDrawArrays绘制两个彼此相连的三角形
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		//GL_TRIANGLES：每三个顶之间绘制三角形，之间不连接
+		//GL_TRIANGLE_FAN：以V0V1V2, V0V2V3, V0V3V4，……的形式绘制三角形
+		//GL_TRIANGLE_STRIP：顺序在每三个顶点之间均绘制三角形。这个方法可以保证从相同的方向上所有三角形均被绘制。以V0V1V2, V1V2V3, V2V3V4……的形式绘制三角形
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	//结束处理
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
+
+	glfwTerminate();
 	return 0;
 }
